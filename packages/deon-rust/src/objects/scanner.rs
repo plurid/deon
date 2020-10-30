d841c59fb9dfdcf011532aa55da0d2bccbfdbcc6
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-10-30 10:27:12 +0200

    deon-rust > dev: scanner

--- packages/deon-rust/src/objects/scanner.rs
+++ packages/deon-rust/src/objects/scanner.rs
@@ -11,6 +11,7 @@ pub struct Scanner {
     start: i64,
     current: i64,
     line: i32,
+    keywords: HashMap<String, TokenType>,
 }
 
 
@@ -18,12 +19,20 @@ impl Scanner {
     pub fn new(
         data: String,
     ) -> Scanner {
+        let mut keywords: HashMap<String, TokenType> = HashMap::new();
+
+        keywords.insert(String::from("import"), TokenType::Import);
+        keywords.insert(String::from("inject"), TokenType::Inject);
+        keywords.insert(String::from("from"), TokenType::From);
+        keywords.insert(String::from("with"), TokenType::With);
+
         Scanner {
             data: data,
             tokens: vec![],
             start: 0,
             current: 0,
             line: 0,
+            keywords,
         }
     }
 
@@ -45,6 +54,8 @@ impl Scanner {
         }
 
         self.end_scan();
+
+        self.identify();
     }
 
     fn scan_token(
@@ -60,9 +71,8 @@ impl Scanner {
             '}' => self.add_token(TokenType::RightCurlyBracket),
 
             ',' => self.add_token(TokenType::Comma),
-            '#' => self.add_token(TokenType::Link),
-            '.' => self.add_token(TokenType::Dot),
-
+            '#' => self.link(),
+            '.' => self.dot(),
             '/' => self.slash(),
             '*' => self.star(),
 
@@ -124,6 +134,88 @@ impl Scanner {
 
 
     // matches
+    fn link(
+        &mut self,
+    ) {
+        if self.match_character('\'') {
+            while self.peek() != '\'' && self.is_at_end() {
+                if self.peek() == '\n' {
+                    self.line += 1;
+
+                    // Error: Unterminated link string.
+                    return;
+                }
+            }
+
+            if self.is_at_end() {
+                // Error: Unterminated link string.
+                return;
+            }
+
+            // The closing '.
+            self.advance();
+
+            // Extract the value without the initial hashstring (#')
+            // and without the last string mark.
+            let value = String::from(
+                &self.data.to_string()[
+                    (self.start + 2) as usize..(self.current - 1) as usize
+                ]
+            );
+
+            self.add_token_literal(
+                TokenType::Link,
+                value,
+            );
+            return;
+        }
+
+        while
+            self.peek() != ' '
+            && self.peek() != '\n'
+            && !self.is_at_end()
+        {
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            // Error: Unterminated link.
+            return;
+        }
+
+        // Extract the value without the initial hash (#).
+        let value = String::from(
+            &self.data.to_string()[
+                (self.start + 1) as usize..(self.current) as usize
+            ]
+        );
+
+        self.add_token_literal(
+            TokenType::Link,
+            value,
+        );
+        return;
+    }
+
+    fn dot(
+        &mut self,
+    ) {
+        if self.match_character('.') {
+            if self.match_character('.') {
+                if self.match_character('#') {
+                    self.spread();
+                } else {
+                    // Error: Can only spread leaflinks.
+                    return;
+                }
+            } else {
+                self.signifier();
+            }
+        } else {
+            self.signifier();
+        }
+    }
+
     fn slash(
         &mut self,
     ) {
@@ -234,7 +326,102 @@ impl Scanner {
 
     fn signifier(
         &mut self,
-    ) {}
+    ) {
+        let mut character = self.peek();
+        while self.is_alpha_numeric(character) {
+            character = self.advance();
+        }
+
+        let value = String::from(
+            &self.data.to_string()[
+                (self.start) as usize..(self.current) as usize
+            ]
+        );
+
+        let mut token_type_option = self.keywords.get(&value);
+        let mut token_type = token_type_option.unwrap_or(&TokenType::Signifier);
+        let mut token_type_clone = token_type.clone();
+
+        let length = self.tokens.len() - 1;
+        let in_group = in_group(
+            self.tokens.clone(),
+            length,
+        );
+
+        match token_type {
+            TokenType::Import | TokenType::Inject | TokenType::From | TokenType::With => {
+                if in_group != "LEAFLINK" {
+                    token_type_clone = TokenType::Signifier;
+                }
+            },
+            _ => (),
+        }
+
+        self.add_token(token_type_clone);
+    }
+
+    fn spread(
+        &mut self,
+    ) {
+        if self.match_character('\'') {
+            // Handle link string spread.
+            while self.peek() != '\'' && self.is_at_end() {
+                if self.peek() == '\n' {
+                    self.line += 1;
+
+                    // Error: Unterminated link string spread.
+                    return;
+                }
+
+                self.advance();
+            }
+
+            // Unterminated link string spread.
+            if self.is_at_end() {
+                return;
+            }
+
+            // The closing '.
+            self.advance();
+
+            // Extract the value without the initial dot hashstring (...#')
+            // and without the last string mark.
+            let value = String::from(
+                &self.data.to_string()[
+                    (self.start + 5) as usize..(self.current - 1) as usize
+                ]
+            );
+
+            self.add_token_literal(
+                TokenType::Spread,
+                value,
+            );
+            return;
+        }
+
+        let mut character = self.peek();
+        while self.is_alpha_numeric(character) {
+            character = self.advance();
+        }
+
+        let value = String::from(
+            &self.data.to_string()[
+                (self.start) as usize..(self.current) as usize
+            ]
+        );
+
+        self.add_token_literal(
+            TokenType::Spread,
+            value,
+        );
+        return;
+    }
+
+    fn identify(
+        &self,
+    ) {
+
+    }
 
 
     // utilities
@@ -285,4 +472,125 @@ impl Scanner {
 
         self.data.chars().nth((self.current) as usize).unwrap()
     }
+
+    fn is_alpha(
+        &mut self,
+        character: char,
+    ) -> bool {
+        character.is_alphabetic()
+    }
+
+    fn is_digit(
+        &mut self,
+        character: char,
+    ) -> bool {
+        character.is_digit(10)
+    }
+
+    fn is_alpha_numeric(
+        &mut self,
+        character: char,
+    ) -> bool {
+        self.is_alpha(character) || self.is_digit(character)
+    }
+
+    fn string_from_signifiers(
+        &self,
+        tokens: Vec<Token>,
+    ) -> Token {
+        let line = tokens[0].line;
+
+        let mut texts = vec![];
+
+        for token in tokens {
+            texts.push(token.lexeme);
+        }
+
+        let lexeme = texts.join(" ");
+        let literal = lexeme.clone();
+
+        Token {
+            token_type: TokenType::String,
+            lexeme,
+            literal,
+            line,
+        }
+    }
+
+    fn identifier_from_signifier(
+        &self,
+        token: Token,
+    ) -> Token{
+        let lexeme = token.lexeme.replace('\'', "");
+        let literal = String::from("");
+        let line = token.line;
+
+        Token {
+            token_type: TokenType::Identifier,
+            lexeme,
+            literal,
+            line,
+        }
+    }
+}
+
+
+fn in_group(
+    tokens_source: Vec<Token>,
+    position: usize,
+) -> String {
+    let mut tokens = tokens_source.clone();
+    tokens.truncate(position);
+    tokens.reverse();
+
+    if tokens.len() == 0 {
+        return String::from("LEAFLINK");
+    }
+
+    let mut curly_brackets: HashMap<String, i32> = HashMap::new();
+    curly_brackets.insert(String::from("left"), 0);
+    curly_brackets.insert(String::from("right"), 0);
+
+    let mut square_brackets: HashMap<String, i32> = HashMap::new();
+    square_brackets.insert(String::from("left"), 0);
+    square_brackets.insert(String::from("right"), 0);
+
+    for token in tokens {
+        match token.token_type {
+            TokenType::LeftCurlyBracket => *curly_brackets.get_mut("left").unwrap() += 1,
+            TokenType::RightCurlyBracket => *curly_brackets.get_mut("right").unwrap() += 1,
+            TokenType::LeftSquareBracket => *square_brackets.get_mut("left").unwrap() += 1,
+            TokenType::RightSquareBracket => *square_brackets.get_mut("right").unwrap() += 1,
+            _ => (),
+        }
+
+        let curly_brackets_left = curly_brackets.get("left").unwrap_or(&0);
+        let curly_brackets_right = curly_brackets.get("right").unwrap_or(&0);
+
+        if curly_brackets_left > curly_brackets_right {
+            return String::from("MAP");
+        }
+
+        let square_brackets_left = square_brackets.get("left").unwrap_or(&0);
+        let square_brackets_right = square_brackets.get("right").unwrap_or(&0);
+
+        if square_brackets_left > square_brackets_right {
+            return String::from("LIST");
+        }
+    }
+
+    let curly_brackets_left = curly_brackets.get("left").unwrap_or(&0);
+    let curly_brackets_right = curly_brackets.get("right").unwrap_or(&0);
+
+    let square_brackets_left = square_brackets.get("left").unwrap_or(&0);
+    let square_brackets_right = square_brackets.get("right").unwrap_or(&0);
+
+    if
+        curly_brackets_left == curly_brackets_right
+        && square_brackets_left == square_brackets_right
+    {
+        return String::from("LEAFLINK");
+    }
+
+    return String::from("");
 }
