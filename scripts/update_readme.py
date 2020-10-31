523210e48137c32b9f9917028052e4535aea18ad
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-10-31 15:10:57 +0200

    scripts > dev: update_readme

---
+++ scripts/update_readme.py
@@ -0,0 +1,34 @@
+import os.path
+
+
+
+def main():
+    root_path = os.path.split(os.path.dirname(__file__))[0]
+
+    readme_paths = [
+        'packages/deon-grammar/vscode/README.md',
+        'packages/deon-javascript/README.md',
+        'packages/deon-rust/README.md',
+    ]
+
+    readme_source_path = os.path.join(
+        root_path,
+        'README.md',
+    )
+    with open(readme_source_path, 'r') as source:
+        readme_data = source.readlines()
+
+
+    for readme_path in readme_paths:
+        readme_update_path = os.path.join(
+            root_path,
+            readme_path,
+        )
+
+        print('Wrote readme in ' + readme_update_path)
+
+        with open(readme_update_path, 'w') as update:
+            update.writelines(readme_data)
+
+
+main()
