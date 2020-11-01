e8b7abe3735dd148ea4f7ade84810a15ff88373b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-11-01 11:14:59 +0200

    deon-javascript > dev: utilities/fetcher/synchronous

---
+++ packages/deon-javascript/source/utilities/fetcher/synchronous/url/index.ts
@@ -0,0 +1,71 @@
+// #region imports
+    // #region libraries
+    import path from 'path';
+
+    import fetch from 'sync-request';
+    // #endregion libraries
+
+
+    // #region external
+    import {
+        FetcherType,
+    } from '../../../../data/interfaces';
+
+    import {
+        fetcherDefaultImportHeaders,
+        fetcherDefaultInjectHeaders,
+
+        DEON_FILENAME_EXTENSION,
+    } from '../../../../data/constants';
+    // #endregion external
+// #endregion imports
+
+
+
+// #region module
+const fetchFromURL = (
+    url: string,
+    token?: string,
+    type?: FetcherType,
+) => {
+    const defaultHeaders = type === 'inject'
+        ? fetcherDefaultInjectHeaders
+        : fetcherDefaultImportHeaders;
+
+    const headers = token
+        ?  {
+            ...defaultHeaders,
+            Authorization: `Bearer ${token}`,
+        } : {
+            ...defaultHeaders,
+        };
+
+    const response = fetch(
+        'GET',
+        url,
+        {
+            headers,
+        },
+    );
+
+    const data = response.getBody();
+
+    const extname = path.extname(url);
+    const filetype = type === 'inject'
+        ? extname || ''
+        : extname || DEON_FILENAME_EXTENSION;
+
+    return {
+        data,
+        filetype,
+    };
+}
+// #endregion module
+
+
+
+// #region exports
+export {
+    fetchFromURL,
+};
+// #endregion exports
