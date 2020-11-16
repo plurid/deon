70efe0903a7cdb64d1e449927ba2a80da17dce12
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-11-16 22:08:53 +0200

    deon-javascript > test: Deon | errors

--- packages/deon-javascript/source/objects/Deon/__tests__/9-errors.test.ts
+++ packages/deon-javascript/source/objects/Deon/__tests__/9-errors.test.ts
@@ -21,7 +21,31 @@
 
 // #region module
 describe(suites.errors, () => {
-    it('various', async () => {
+    it('no root line', async () => {
+        const dataValues = `a`;
+
+        const start = Date.now();
+        const deon = new Deon();
+        const data = await deon.parse(
+            dataValues,
+        );
+        const end = Date.now();
+        // log(data);
+
+
+        expect(Object.keys(data).length).toEqual(0);
+
+        compareTimeBenchmark(
+            start,
+            end,
+            'instant',
+            `${suites.errors} - no root line`,
+        );
+    });
+
+
+
+    it('no root', async () => {
         const dataValues = `
 a
         `;
@@ -35,11 +59,40 @@ a
         // log(data);
 
 
+        expect(Object.keys(data).length).toEqual(0);
+
         compareTimeBenchmark(
             start,
             end,
             'instant',
-            `${suites.errors} - various`,
+            `${suites.errors} - no root`,
+        );
+    });
+
+
+
+    it('no closing bracket', async () => {
+        const dataValues = `
+{
+    key value
+        `;
+
+        const start = Date.now();
+        const deon = new Deon();
+        const data = await deon.parse(
+            dataValues,
+        );
+        const end = Date.now();
+        // log(data);
+
+
+        expect(Object.keys(data).length).toEqual(0);
+
+        compareTimeBenchmark(
+            start,
+            end,
+            'instant',
+            `${suites.errors} - no closing bracket`,
         );
     });
 });
