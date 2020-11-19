419715d6745b122daae18d1d576a3f56e50c8dd2
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-11-19 12:27:07 +0200

    deon-javascript > setup: package

--- packages/deon-javascript/source/data/constants/index.ts
+++ packages/deon-javascript/source/data/constants/index.ts
@@ -10,7 +10,7 @@
 
 
 // #region module
-const DEON_CLI_VERSION = '0.0.41';
+const DEON_CLI_VERSION = '0.0.42';
 
 const DEON_FILENAME_EXTENSION = '.deon';
 const DEON_MEDIA_TYPE = 'application/deon';
