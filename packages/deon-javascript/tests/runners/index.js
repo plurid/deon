4c0005df910763663d713c5af6b9bd20c90cebe3
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-11-20 09:11:22 +0200

    deon-javascript > test: runners

--- packages/deon-javascript/tests/runners/index.js
+++ packages/deon-javascript/tests/runners/index.js
@@ -18,15 +18,51 @@ const run = async (
     value,
 ) => {
     try {
+        const start = Date.now();
+
         const deon = new Deon();
         const parsed = await deon.parse(data);
+
+        const end = Date.now();
 
         expect(
             parsed[access],
             value,
         );
+
+        timeBenchmark(
+            start,
+            end,
+            'instant',
+            `name`,
+        );
     } catch (error) {
         console.log(error);
+    }
+}
+
+
+const timeBenchmark = (
+    start,
+    end,
+    kind, // 'instant' | 'fast' | 'network'
+    id,
+) => {
+    const timeBenchmarkValues = {
+        instant: 0,
+        fast: 10,
+        network: 500,
+    };
+    const timeTolerance = 5;
+
+    const duration = end - start;
+    const benchmark = typeof timeBenchmarkValues[kind] !== 'undefined'
+        ? timeBenchmarkValues[kind]
+        : timeBenchmarkValues.instant;
+    const maximum = benchmark + timeTolerance;
+
+    if (duration > maximum) {
+        console.log(`\n\tExecution time of '${id}' exceeded: (${duration} ms instead of ${maximum} ms).`);
     }
 }
 
