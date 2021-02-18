e198e0a76ea40df1c25f1eca0ba35a811e9d20b9
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-02-18 08:58:12 +0200

    deon-javascript > dev: data/enumerations

--- packages/deon-javascript/source/data/enumerations/index.ts
+++ packages/deon-javascript/source/data/enumerations/index.ts
@@ -3,6 +3,7 @@ export enum TokenType {
     // Single-character tokens.
     LEFT_SQUARE_BRACKET, RIGHT_SQUARE_BRACKET,
     LEFT_CURLY_BRACKET, RIGHT_CURLY_BRACKET,
+    LEFT_PARENTHESIS, RIGHT_PARENTHESIS,
     COMMA, DOT,
 
     // Multi-character tokens.
