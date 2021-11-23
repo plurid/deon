8e1f86113683cff14e9775086aaee73d6e532b74
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-11-23 18:09:58 +0200

    deon-c > dev: cli

--- packages/deon-c/source/main.c
+++ packages/deon-c/source/main.c
@@ -1,4 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
 
 
 
@@ -6,11 +8,33 @@ int main(
     int argc,
     char* argv[]
 ) {
-    printf("arguments number: %d", argc);
-    printf("\n");
+    int c;
 
-    for (int i = 0; i < argc; i++) {
-        printf("argument: %s", argv[i]);
-        printf("\n");
+    while (1) {
+        static struct option long_options[] = {
+            {"version", no_argument, 0, 1},
+            {"output", no_argument, 0, 1},
+            {"typed", no_argument, 0, 1},
+            {"help", no_argument, 0, 1},
+        };
+
+        /* getopt_long stores the option index here. */
+        int option_index = 0;
+
+        c = getopt_long(
+            argc,
+            argv,
+            "abc:d:f:",
+            long_options,
+            &option_index
+        );
+
+        switch (c) {
+            default:
+                printf("%d", c);
+                break;
+        }
+
+        break;
     }
 }
