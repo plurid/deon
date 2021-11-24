a3fa2fa7f9d50c9de11f2f42b182fb5f533d4f9c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-11-24 17:45:44 +0200

    deon-c > dev: source

--- packages/deon-c/source/cli/main.h
+++ packages/deon-c/source/cli/main.h
@@ -1,3 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+
+
+
+int read_arguments(
+    int argc,
+    char* argv[]
+);
--- packages/deon-c/source/main.c
+++ packages/deon-c/source/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#include "cli/main.c"
+#include "cli/main.h"
 
 
 
