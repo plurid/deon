fc72b6b80903512e541b96b12a1422178bdf9b57
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-11-24 12:34:31 +0200

    deon-c > dev: cli

---
+++ packages/deon-c/source/cli/main.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+
+
+int read_arguments(
+    int argc,
+    char* argv[]
+) {
+    printf("reading arguments");
+    return 1;
+
+    // int c;
+
+    // while (1) {
+    //     static struct option long_options[] = {
+    //         {"version", no_argument, 0, 1},
+    //         {"output", no_argument, 0, 1},
+    //         {"typed", no_argument, 0, 1},
+    //         {"help", no_argument, 0, 1},
+    //     };
+
+    //     /* getopt_long stores the option index here. */
+    //     int option_index = 0;
+
+    //     c = getopt_long(
+    //         argc,
+    //         argv,
+    //         "abc:d:f:",
+    //         long_options,
+    //         &option_index
+    //     );
+
+    //     switch (c) {
+    //         default:
+    //             printf("%d", c);
+    //             break;
+    //     }
+
+    //     break;
+    // }
+}
--- packages/deon-c/source/main.c
+++ packages/deon-c/source/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <getopt.h>
+
+#include "cli/main.c"
 
 
 
@@ -8,33 +8,6 @@ int main(
     int argc,
     char* argv[]
 ) {
-    int c;
-
-    while (1) {
-        static struct option long_options[] = {
-            {"version", no_argument, 0, 1},
-            {"output", no_argument, 0, 1},
-            {"typed", no_argument, 0, 1},
-            {"help", no_argument, 0, 1},
-        };
-
-        /* getopt_long stores the option index here. */
-        int option_index = 0;
-
-        c = getopt_long(
-            argc,
-            argv,
-            "abc:d:f:",
-            long_options,
-            &option_index
-        );
-
-        switch (c) {
-            default:
-                printf("%d", c);
-                break;
-        }
-
-        break;
-    }
+    int arguments = read_arguments(argc, argv);
+    printf("arguments %d", arguments);
 }
